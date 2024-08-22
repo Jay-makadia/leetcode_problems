3c033975370c5617d90b9d5152e0de44cf62c9be
Jay-makadia <144011652+Jay-makadia@users.noreply.github.com>
2024-08-22 18:52:30 +0530

    Time: 1 ms (26.96%) | Memory: 40.3 MB (67.03%) - LeetSync

---
+++ 476-number-complement/number-complement.java
@@ -0,0 +1,34 @@
+class Solution {
+    public static int pow(int a, int b) {
+        int an = 1;
+        for (int i = 0; i < b; i++) {
+            an *= a;
+        }
+        return an;
+    }
+
+    public int findComplement(int num) {
+        String s = Integer.toBinaryString(num);
+        int n = s.length();
+
+        // Using StringBuilder to modify the string
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < n; i++) {
+            if (sb.charAt(i) == '1') {
+                sb.setCharAt(i, '0');
+            } else {
+                sb.setCharAt(i, '1');
+            }
+        }
+
+        // Convert the modified binary string back to an integer
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (sb.charAt(i) == '1') {
+                ans += pow(2, n - 1 - i); // Corrected the power calculation
+            }
+        }
+
+        return ans;
+    }
+}
